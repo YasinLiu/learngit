1f9bade58235fee030d2afbae57de1707557465e
Yasin <yasinliu615@mial.com>
2019-03-28 15:46:55 +0500

    this is a test demo

---
+++ demo.py
@@ -0,0 +1,13 @@
+coefficient = 0.6
+
+result = 0
+def distance_sum(init_height, times):
+    if times > 0:
+        times -= 1
+        result = init_height + init_height*coefficient + distance_sum(init_height*coefficient, times)
+    return result
+
+
+if __name__ == '__main__':
+    s = distance_sum(10, 4)
+    print(s)
