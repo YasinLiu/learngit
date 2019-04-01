98af2aaf7ac4aaad0dca16bab2496b0561117723
Yasin_karot <yasinliu615@mial.com>
2019-04-01 22:38:19 +0500

    solve problem 1.3

--- demo.py
+++ demo.py
@@ -10,12 +10,14 @@ coefficient = 0.6
 
 result = 0
 def distance_sum(init_height, times):
-    if times > 0:
+    if times < 0:
+        return 0
+    else:
         times -= 1
         result = init_height + init_height*coefficient + distance_sum(init_height*coefficient, times)
-    return result
+        return result
 
 
 if __name__ == '__main__':
-    s = distance_sum(10, 4)
+    s = distance_sum(10, 2)
     print(s)
